5cb77701453a1178c19e2380c33554a2aa31ece9
Riccardo Zappa <rzappa00@gmail.com>
2025-07-01 10:59:05 +0300

    commented the import of yolo ros

--- app/mask2former_ros_node.py
+++ app/mask2former_ros_node.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import hydra_seg_ros.nodes.yolo_ros as yolo_ros
+# import hydra_seg_ros.nodes.yolo_ros as yolo_ros
 import hydra_seg_ros.nodes.mask2former_ros as mask2former_ros
 
 if __name__ == "__main__":
