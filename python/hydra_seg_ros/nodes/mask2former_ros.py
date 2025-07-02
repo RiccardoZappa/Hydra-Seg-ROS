2e49c0cd977a571ac453e8e4eec70e9557566236
Riccardo Zappa <rzappa00@gmail.com>
2025-07-02 11:43:42 +0300

    refactoring to grab the coco classed from a yaml configuration file

--- python/hydra_seg_ros/nodes/mask2former_ros.py
+++ python/hydra_seg_ros/nodes/mask2former_ros.py
@@ -23,145 +23,6 @@ from hydra_msgs.msg import HydraVisionPacket, Masks
 # Utility Imports
 from hydra_seg_ros.utils import ros_utils
 
-
-COCO_PANOPTIC_CLASSES = {
-    # --- "Things" - Objects with Instances ---
-    0: 'person',
-    1: 'bicycle',
-    2: 'car',
-    3: 'motorcycle',
-    4: 'airplane',
-    5: 'bus',
-    6: 'train',
-    7: 'truck',
-    8: 'boat',
-    9: 'traffic light',
-    10: 'fire hydrant',
-    11: 'stop sign',
-    12: 'parking meter',
-    13: 'bench',
-    14: 'bird',
-    15: 'cat',
-    16: 'dog',
-    17: 'horse',
-    18: 'sheep',
-    19: 'cow',
-    20: 'elephant',
-    21: 'bear',
-    22: 'zebra',
-    23: 'giraffe',
-    24: 'backpack',
-    25: 'umbrella',
-    26: 'handbag',
-    27: 'tie',
-    28: 'suitcase',
-    29: 'frisbee',
-    30: 'skis',
-    31: 'snowboard',
-    32: 'sports ball',
-    33: 'kite',
-    34: 'baseball bat',
-    35: 'baseball glove',
-    36: 'skateboard',
-    37: 'surfboard',
-    38: 'tennis racket',
-    39: 'bottle',
-    40: 'wine glass',
-    41: 'cup',
-    42: 'fork',
-    43: 'knife',
-    44: 'spoon',
-    45: 'bowl',
-    46: 'banana',
-    47: 'apple',
-    48: 'sandwich',
-    49: 'orange',
-    50: 'broccoli',
-    51: 'carrot',
-    52: 'hot dog',
-    53: 'pizza',
-    54: 'donut',
-    55: 'cake',
-    56: 'chair',
-    57: 'couch',
-    58: 'potted plant',
-    59: 'bed',
-    60: 'dining table',
-    61: 'toilet',
-    62: 'tv',
-    63: 'laptop',
-    64: 'mouse',
-    65: 'remote',
-    66: 'keyboard',
-    67: 'cell phone',
-    68: 'microwave',
-    69: 'oven',
-    70: 'toaster',
-    71: 'sink',
-    72: 'refrigerator',
-    73: 'book',
-    74: 'clock',
-    75: 'vase',
-    76: 'scissors',
-    77: 'teddy bear',
-    78: 'hair drier',
-    79: 'toothbrush',
-    # --- "Stuff" - Background Regions ---
-    80: 'banner',
-    81: 'blanket',
-    82: 'bridge',
-    83: 'cardboard',
-    84: 'counter',
-    85: 'curtain',
-    86: 'door-stuff',
-    87: 'floor-wood',
-    88: 'flower',
-    89: 'fruit',
-    90: 'gravel',
-    91: 'house',
-    92: 'light',
-    93: 'mirror-stuff',
-    94: 'net',
-    95: 'pillow',
-    96: 'platform',
-    97: 'playingfield',
-    98: 'railroad',
-    99: 'river',
-    100: 'road',
-    101: 'roof',
-    102: 'sand',
-    103: 'sea',
-    104: 'shelf',
-    105: 'snow',
-    106: 'stairs',
-    107: 'tent',
-    108: 'towel',
-    109: 'wall-brick',
-    110: 'wall-stone',
-    111: 'wall-tile',
-    112: 'wall-wood',
-    113: 'water-other',
-    114: 'window-blind',
-    115: 'window-other',
-    116: 'tree-merged',
-    117: 'fence-merged',
-    118: 'ceiling-merged',
-    119: 'sky-other-merged',
-    120: 'cabinet-merged',
-    121: 'table-merged',
-    122: 'floor-other-merged',
-    123: 'pavement-merged',
-    124: 'mountain-merged',
-    125: 'grass-merged',
-    126: 'dirt-merged',
-    127: 'paper-merged',
-    128: 'food-other-merged',
-    129: 'building-other-merged',
-    130: 'rock-merged',
-    131: 'wall-other-merged',
-    132: 'rug-merged',
-}
-
 class Mask2FormerRosNode:
     """
     A ROS node to perform panoptic segmentation using a Mask2Former model
@@ -176,9 +37,8 @@ class Mask2FormerRosNode:
     def create_color_map(self):
         # Creates a consistent color for every possible semantic class
         np.random.seed(42)
-        num_classes = len(COCO_PANOPTIC_CLASSES)
         # We make it large enough for any potential ID
-        color_map = np.random.randint(0, 255, size=(num_classes * 2, 3), dtype=np.uint8)
+        color_map = np.random.randint(0, 255, size=(self.num_classes * 2, 3), dtype=np.uint8)
         color_map[0] = [0, 0, 0] # Ensure background is black
         return color_map
 
@@ -194,10 +54,12 @@ class Mask2FormerRosNode:
         self.conf_threshold = rospy.get_param("~conf_threshold", 0.5)
         self.label_space_file = rospy.get_param(
             "~label_space_file",
-            "/home/ros/hydra_ws/src/hydra_stretch/config/label_spaces/coco_kitchen_large_objects_label_space.yaml",
+            "/home/ros/hydra_ws/src/hydra_stretch/config/label_spaces/coco_flat_panoptic_label_space.yaml",
         )
         with open(str(self.label_space_file), "r") as f:
-            self.label_space = yaml.load(f, Loader=Loader)["object_labels"]
+            data = yaml.load(f, Loader=Loader)
+            self.label_space = data["object_labels"] + data["structure_labels"]
+            self.num_classes = data["total_semantic_labels"]
 
         self.panoptic_id_multiplier = 1000 
 
